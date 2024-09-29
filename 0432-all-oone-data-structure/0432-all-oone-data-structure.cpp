a184fc5619f976242a3c1dffe90aa42846fee476
Rishab Yadav <115487678+iamrishaby@users.noreply.github.com>
2024-09-29 19:22:24 +0530

    Time: 2855 ms (5.09%), Space: 48.2 MB (100.00%) - LeetHub

---
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -0,0 +1,46 @@
+class AllOne {
+public:
+    unordered_map<string, int> freq;
+    AllOne() {
+    }
+    
+    void inc(string key) {
+        freq[key]++;
+    }
+    
+    void dec(string key) {
+        freq[key]--;
+    }
+    
+    string getMaxKey() {
+        int mx = 0;
+        string ans = "";
+        for(auto& val:freq){
+            if(val.second > mx){
+                mx = val.second;
+                ans = val.first;
+            }
+        }
+        return ans;
+    }
+    
+    string getMinKey() {
+        int mini = INT_MAX;
+        string ans = "";
+        for(auto& val:freq){
+            if(val.second!=0 and val.second < mini){
+                mini = val.second;
+                ans = val.first;
+            }
+        }
+        return ans;
+    }
+};
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne* obj = new AllOne();
+ * obj->inc(key);
+ * obj->dec(key);
+ * string param_3 = obj->getMaxKey();
+ * string param_4 = obj->getMinKey();
+ */
